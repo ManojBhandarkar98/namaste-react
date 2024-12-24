00785400449852f078c47800e1730082ee3b26ca
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2024-12-24 22:06:32 +0530

    jsx, react Elemetn, react component rendering

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//react Element
 const jsxHeading = (<h1 id="heading">Namaste React using JSX 🚀</h1>);
-//in single line JSX no need bracket
-//if multiline required bracket
 
+const Title = () => (
+    <h1 id="heading">Namaste React using JSX 🚀</h1>
+);
+
+//React Functional Component
+const Heading = () => {
+    return (
+        <div id="container">
+            <Title />       //React functional component render in heading component    OR Component Composition
+            {jsxHeading}    //javascript code we can excute in JSX
+            <h1 id="heading">Namaste React using JSX 🚀</h1>
+        </div>);
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Heading />);   //render react component - Component first letter Capital
+//root.render(jsxHeading);    //render react Element
