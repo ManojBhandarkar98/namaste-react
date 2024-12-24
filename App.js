8f5f6c3a400acba7bcec5ec524fc6bad7895df74
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2024-12-24 23:22:25 +0530

    Complete Header Component

--- App.js
+++ App.js
@@ -4,19 +4,33 @@ import ReactDOM from "react-dom/client";
 //react Element
 const jsxHeading = (<h1 id="heading">Namaste React using JSX 🚀</h1>);
 
-const Title = () => (
-    <h1 id="heading">Namaste React using JSX 🚀</h1>
-);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
 
 //React Functional Component
-const Heading = () => {
+const AppLayout = () => {
     return (
-        <div id="container">
-            <Title />       //React functional component render in heading component    OR Component Composition
-            {jsxHeading}    //javascript code we can excute in JSX
-            <h1 id="heading">Namaste React using JSX 🚀</h1>
+        <div className="app">
+            <Header />
+
+
         </div>);
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading />);   //render react component - Component first letter Capital
+root.render(<AppLayout />);   //render react component - Component first letter Capital
 //root.render(jsxHeading);    //render react Element
