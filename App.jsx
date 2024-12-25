984ac90944bd98b0a0c6b0f395f9cac5f12ca921
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2024-12-25 22:56:21 +0530

    css apply on resto card

--- App.jsx
+++ App.jsx
@@ -22,13 +22,38 @@ const Header = () => {
     );
 }
 
+const RestroCard = () => {
+    return (
+        <div className="restro-card" style={{backgroundColor:"#f0f0f0"}}>
+            <img className="rest-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/yzgqriufpzmloogcn2vl" alt="rest-logo" />
+            <h2>Meghna Foods</h2>
+            sdsd
+        </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="wrapper">
+            <div className="Search-container">
+                Search
+            </div>
+           
+            <div className="restro-container">
+            <RestroCard />
+            </div>
+
+        </div>
+    );
+}
+
 //React Functional Component
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-
-
+            <Body />
+           
         </div>);
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
