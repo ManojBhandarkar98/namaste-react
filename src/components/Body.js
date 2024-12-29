3e2ed13e57161de63623d1e2d043b3978025cc0a
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2024-12-29 17:12:25 +0530

    Epi 06 - Shimmer UI Effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,31 @@
 import { useEffect, useState } from "react";
 import RestroCard from "./RestroCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
     const [listOfRestaurants, setListOfRestaurants] = useState([]);     //array return , defualt value is empty array we pass behave normal var
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-    },[])
+    }, [])
 
-    const fetchData = async ()=>{
+    const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json()
-    
+
         setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
     }
+
+    if (listOfRestaurants.length == 0)
+        return <Shimmer />;
 
     return (
         <div className="wrapper">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
                     const filteredList = listOfRestaurants.filter(
-                        (res) => res.info.avgRating > 4
+                        (res) => res.data.avgRating > 4
                     );
                     setListOfRestaurants(filteredList);
                 }}>Top Rated Restaurant </button>
@@ -30,7 +34,7 @@ const Body = () => {
             <div className="restro-container">
                 {
                     listOfRestaurants.map(restaurant =>
-                        <RestroCard key={restaurant.info.id} restData={restaurant} />)
+                        <RestroCard key={restaurant.data.id} restData={restaurant} />)
                 }
             </div>
 
