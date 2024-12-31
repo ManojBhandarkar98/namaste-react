93ddda700c80a845fc0e11720bdbd57b4aecaf19
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2024-12-31 16:43:22 +0530

    Epi - 07 - Route page added about, home, contact

--- App.jsx
+++ App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
-
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 //react Element
 const jsxHeading = (<h1 id="heading">Namaste React using JSX 🚀</h1>);
@@ -16,6 +18,23 @@ const AppLayout = () => {
             <Body/>
         </div>);
 }
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />
+    },
+    {
+        path: "/about",
+        element: <About/>
+    },
+    {
+        path:"/contact",
+        element: <Contact/>
+    }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);   //render react component - Component first letter Capital
+root.render(<RouterProvider router = {appRouter} />); 
+  //render react component - Component first letter Capital
 //root.render(jsxHeading);    //render react Element
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+const Contact = () => {
+    return (<div>
+        <h1>Contact</h1>
+        <h3 > 8788796066</h3>
+    </div>);
+}
+export default Contact;
