fc741476c43082ea9a2b61d4def74e6d16fa9ca3
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2024-12-31 17:32:10 +0530

    Epi - 07 - Link with Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 const Header = () => {
     let btnName="Login";
     const [btnNameReact,setBtnNameReact] = useState("Login");
@@ -15,9 +16,9 @@ const Header = () => {
             <div className="nav-items">
                 <ul>
                     mamaoks
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                     <li>Cart</li>
                     <button className="login" onClick={()=>{
                     btnNameReact=="Login"? setBtnNameReact("Logout"):setBtnNameReact("Login");
